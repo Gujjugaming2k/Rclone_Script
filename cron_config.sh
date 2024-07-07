505e70906ae016642372b05324866f657c846af1
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2024-07-07 22:17:53 +0530

    Update cron_config.sh

--- cron_config.sh
+++ cron_config.sh
@@ -6,6 +6,6 @@ id
 id
 sudo apt-get update -y && sudo apt-get install cron -y
 sudo service cron start
-(crontab -l | grep -v "0 2 * * * /opt/zip_backup.sh") | crontab -
+(sudo crontab -l | echo "0 2 * * * /opt/zip_backup.sh") | sudo crontab -
 sudo wget -O /opt/zip_backup.sh https://raw.githubusercontent.com/Gujjugaming2k/Rclone_Script/main/zip_backup.sh
 sudo chmod 777 /opt/zip_backup.sh
