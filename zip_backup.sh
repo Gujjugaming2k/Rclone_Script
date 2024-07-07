12e17fad3f48e0957b48df26743b93c714a495c9
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2024-07-07 22:08:58 +0530

    Create zip_backup.sh

---
+++ zip_backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo su - root
+id
+id
+id
+id
+echo "Zipping the directory SOURCE_DIR to BACKUP_FILE..."
+echo "Zipping the directory SOURCE_DIR to BACKUP_FILE..."
+echo "Zipping the directory SOURCE_DIR to BACKUP_FILE..."
+echo "Zipping the directory SOURCE_DIR to BACKUP_FILE..."
+
+sudo zip -r jellyfin_backup.zip /opt/jellyfin/
+
+
+echo "Moving BACKUP_FILE to DEST_DIR..."
+echo "Moving BACKUP_FILE to DEST_DIR..."
+echo "Moving BACKUP_FILE to DEST_DIR..."
+echo "Moving BACKUP_FILE to DEST_DIR..."
+echo "Moving BACKUP_FILE to DEST_DIR..."
+sudo mv /opt/jellyfin/jellyfin_backup.zip /opt/drive_bkp/
