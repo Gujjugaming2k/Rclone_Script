4ad44877bb176e847c70652fa5ffff0be1d48595
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2024-07-08 15:25:54 +0530

    Update Jellyfin_Install.sh

--- Jellyfin_Install.sh
+++ Jellyfin_Install.sh
@@ -74,4 +74,4 @@ sudo dpkg -i cloudflared.deb &&
 
 sudo cloudflared service install eyJhIjoiNWIzNDA1ZDEzZmJiNWE1M2I2ZjM5ZjU4M2YwZmYwNjEiLCJ0IjoiOGQ4NWZjODYtMGQwZC00MTFhLWE1Y2EtZjc1NDliZWZiNTQ4IiwicyI6Ik9UZ3hOV1EwTWprdE9HRTVaQzAwTVRFeUxXSmhZelF0WkdNMU9EVTRaR0V6WWpVMyJ9
 
-sudo /opt/jellyfin/jellyfin.sh
+nohup sudo /opt/jellyfin/jellyfin.sh &
