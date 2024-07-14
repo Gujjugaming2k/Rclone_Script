6a700f51c80cc7f7d33a78a10db10e1716901ffa
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2024-07-14 15:57:50 +0530

    Update zip_backup.sh

--- zip_backup.sh
+++ zip_backup.sh
@@ -9,7 +9,7 @@ echo "Zipping the directory SOURCE_DIR to BACKUP_FILE..."
 echo "Zipping the directory SOURCE_DIR to BACKUP_FILE..."
 echo "Zipping the directory SOURCE_DIR to BACKUP_FILE..."
 
-rm -rf /opt/jellyfin/jellyfin_backup.zip
+sudo rm -rf /opt/jellyfin/jellyfin_backup.zip
 
 sudo zip -r /opt/jellyfin/jellyfin_backup.zip /opt/jellyfin/*
 
