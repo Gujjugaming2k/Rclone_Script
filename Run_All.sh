942a0d83bb982ecac05de073c9500d076456d38c
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2024-07-16 20:58:08 +0530

    Update Run_All.sh

--- Run_All.sh
+++ Run_All.sh
@@ -4,20 +4,9 @@
 echo "Running cron_config.sh..."
 curl -sSL https://raw.githubusercontent.com/Gujjugaming2k/Rclone_Script/main/cron_config.sh | sudo bash
 
-# Step 2: Run rclone_script.sh
-echo "Running rclone_script.sh..."
-curl -sSL https://raw.githubusercontent.com/Gujjugaming2k/Rclone_Script/main/rclone_script.sh | sudo bash
-
-# Step 3-1: Run Rclone_Config.sh (assuming it should be run once)
-echo "Running Rclone_Config.sh... 1 time"
-curl -sSL https://raw.githubusercontent.com/Gujjugaming2k/Rclone_Script/main/Rclone_Config.sh | sudo bash
-
-# Step 3-2: Run Rclone_Config.sh (assuming it should be run 2nd time)
-echo "Running Rclone_Config.sh... 2nd time"
-curl -sSL https://raw.githubusercontent.com/Gujjugaming2k/Rclone_Script/main/Rclone_Config.sh | sudo bash
 
 
-# Step 4: Run Jellyfin_Install.sh
+# Step 2: Run Jellyfin_Install.sh
 echo "Running Jellyfin_Install.sh..."
 curl -sSL https://raw.githubusercontent.com/Gujjugaming2k/Rclone_Script/main/Jellyfin_Install.sh | sudo bash
 
