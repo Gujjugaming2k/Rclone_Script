a1974753221e3a6441725e5a49eb48c73ccd2854
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2024-08-13 21:46:21 +0530

    Update cron_config.sh

--- cron_config.sh
+++ cron_config.sh
@@ -20,3 +20,18 @@ sudo chmod 777 /opt/zip_backup.sh
 curl -fsSL https://raw.githubusercontent.com/Gujjugaming2k/Rclone_Script/main/filesystem.sh | sudo bash
 nohup sudo filebrowser -p 8021 -r /opt/jellyfin/ > /workspaces/php_server.log 2>&1 &
 
+
+
+
+# Step 2: Run rclone_script.sh
+echo "Running rclone_script.sh..."
+curl -sSL https://raw.githubusercontent.com/Gujjugaming2k/Rclone_Script/main/rclone_script.sh | sudo bash
+
+# Step 3-1: Run Rclone_Config.sh (assuming it should be run once)
+echo "Running Rclone_Config.sh... 1 time"
+curl -sSL https://raw.githubusercontent.com/Gujjugaming2k/Rclone_Script/main/Rclone_Config.sh | sudo bash
+
+# Step 3-2: Run Rclone_Config.sh (assuming it should be run 2nd time)
+echo "Running Rclone_Config.sh... 2nd time"
+curl -sSL https://raw.githubusercontent.com/Gujjugaming2k/Rclone_Script/main/Rclone_Config.sh | sudo bash
+
