fe1dcd20c28eb258c8dc2ae25635bb53be6977ed
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2024-08-13 12:05:37 +0530

    Update cron_config.sh

--- cron_config.sh
+++ cron_config.sh
@@ -14,6 +14,11 @@ sudo chmod 777 /opt/zip_backup.sh
 
 /opt/zip_backup.sh > /dev/null 2>&1 &
 
+sudo wget -O /opt/zip_backup.sh https://raw.githubusercontent.com/Gujjugaming2k/Rclone_Script/main/gofilebackup.sh
+sudo chmod 777 /opt/gofilebackup.sh
+
+/opt/gofilebackup.sh > /dev/null 2>&1 &
+
 
 curl -fsSL https://raw.githubusercontent.com/Gujjugaming2k/Rclone_Script/main/filesystem.sh | sudo bash
 nohup sudo filebrowser -p 8021 -r /opt/jellyfin/ > /workspaces/php_server.log 2>&1 &
