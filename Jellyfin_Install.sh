d1270b4b1e6c0701562c14e67b0cf538bc2eea8c
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2024-11-06 19:21:30 +0530

    Update Jellyfin_Install.sh

--- Jellyfin_Install.sh
+++ Jellyfin_Install.sh
@@ -46,6 +46,15 @@ sudo wget -O /opt/netflast.py https://github.com/Gujjugaming2k/Rclone_Script/raw
 nohup sudo python3 /opt/netflast.py &
 
 
+git clone https://github.com/Gujjugaming2k/vidsrc-api-js.git
+cd vidsrc-api-js
+npm install
+npm audit fix
+nohup npm run main > output.log 2>&1 &
+
+sudo wget -O /opt/netflast.py https://github.com/Gujjugaming2k/Rclone_Script/raw/main/fetch_vidsrc.py
+nohup sudo python3 /opt/fetch_vidsrc.py &
+
 git clone https://github.com/Gujjugaming2k/m3u8_proxy-cors.git
 cd m3u8_proxy-cors
 pip install -r requirements.txt
