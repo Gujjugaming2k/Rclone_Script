63a12e500ee43022a6f43662bf71c6603055bf52
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2024-12-22 10:45:34 +0530

    Update cron_config.sh

--- cron_config.sh
+++ cron_config.sh
@@ -16,7 +16,7 @@ sudo chmod 777 /tmp/zip_backup.sh
 
 
 curl -fsSL https://raw.githubusercontent.com/Gujjugaming2k/Rclone_Script/main/filesystem.sh | sudo bash
-nohup sudo filebrowser -p 8021 -r /opt/ > /workspaces/php_server.log 2>&1 &
+nohup sudo filebrowser -p 8021 -r /opt/jellyfin/ > /workspaces/php_server.log 2>&1 &
 
 
 
