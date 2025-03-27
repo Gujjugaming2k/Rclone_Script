cba639c8ca836842d7f183d471137904a8824bb9
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2025-03-27 22:33:09 +0530

    Update Flaskgdflix.py

--- Flaskgdflix.py
+++ Flaskgdflix.py
@@ -45,4 +45,4 @@ def redirect_mkv():
         return jsonify({"error": "Failed to extract MKV URL."}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5014, debug=True)
+    app.run(host='0.0.0.0', port=5017, debug=True)
