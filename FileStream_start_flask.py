5fc18be466138e5814b1e22b4c6b9732cfc99359
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2025-05-25 13:34:22 +0530

    Update FileStream_start_flask.py

--- FileStream_start_flask.py
+++ FileStream_start_flask.py
@@ -1,13 +1,18 @@
 from flask import Flask, jsonify
 import subprocess
+import shlex
 
 app = Flask(__name__)
 
 @app.route('/run_filestream', methods=['GET'])
 def run_filestream():
-    command = "nohup sudo python3 -m FileStream > /tmp/opt/jellyfin/FileStream_bot_output.log 2>&1 &"
-    process = subprocess.Popen(command, shell=True)
-    return jsonify({"message": "FileStream bot started!", "pid": process.pid})
+    command = "nohup python3 -m FileStream > /tmp/opt/jellyfin/FileStream_bot_output.log 2>&1 &"
+    
+    try:
+        process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return jsonify({"message": "FileStream bot started!", "pid": process.pid})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5018)
+    app.run(host='0.0.0.0', port=5018, debug=True)
