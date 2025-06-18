211482c81b0e8076723c8c33bffc87911226ab23
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2025-06-18 11:08:22 +0530

    Update filesystem.sh

--- filesystem.sh
+++ filesystem.sh
@@ -4,12 +4,3 @@
 echo "Installing File Browser..."
 curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | sudo bash
 
-# Initialize configuration
-echo "Initializing configuration..."
-sudo filebrowser config init
-
-# Create admin user
-echo "Adding admin user..."
-sudo filebrowser users add admin1 VFlix1 --perm.admin
-
-echo "✅ File Browser setup complete!"
