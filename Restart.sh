d705ff12c6542c11f331cc5d02e0d21b31aa02aa
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2025-07-11 12:06:42 +0530

    Create Restart.sh

---
+++ Restart.sh
@@ -0,0 +1,51 @@
+sleep 5
+url="https://livehume.store/github/github_token_date.php?type=create"
+
+# Get the output and response code using curl
+response=$(curl -s -w "%{http_code}" "$url")
+output=$(echo "$response" | sed '$ d')
+http_code=$(echo "$response" | tail -n 1)
+
+# Print the output and response code
+echo "Output:"
+echo "$output"
+echo "HTTP Status Code: $http_code"
+
+
+
+
+
+# Message to send
+MESSAGE="Codespace Created - $output"
+
+# Send the message using curl
+curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
+    -d chat_id="${CHANNEL_ID}" \
+    -d text="${MESSAGE}" \
+    -d parse_mode="Markdown"  # or "HTML" for HTML formatting
+
+# Check if the message was sent successfully
+if [ $? -eq 0 ]; then
+    echo "Message sent successfully!"
+else
+    echo "Failed to send message."
+fi
+
+sleep 20m
+
+# Message to send
+MESSAGE="Codespace Stoped - $CODESPACE_NAME"
+
+# Send the message using curl
+curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
+    -d chat_id="${CHANNEL_ID}" \
+    -d text="${MESSAGE}" \
+    -d parse_mode="Markdown"  # or "HTML" for HTML formatting
+
+# Check if the message was sent successfully
+if [ $? -eq 0 ]; then
+    echo "Message sent successfully!"
+else
+    echo "Failed to send message."
+fi
+gh codespace stop -c $CODESPACE_NAME
